07a22339ae2ebd467bfd14ab4d61f47564505e25
Mark Zorkhin <mark.zorikhin@dif.tech>
2025-05-06 11:23:39 +0300

    Update category icon

--- pinfin/projector/src/commonMain/kotlin/hnau/pinfin/projector/utils/category/CategoryDirectionExt.kt
+++ pinfin/projector/src/commonMain/kotlin/hnau/pinfin/projector/utils/category/CategoryDirectionExt.kt
@@ -1,13 +1,13 @@
 package hnau.pinfin.projector.utils.category
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowCircleDown
-import androidx.compose.material.icons.filled.ArrowCircleUp
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DoNotDisturbOn
 import androidx.compose.ui.graphics.vector.ImageVector
 import hnau.pinfin.data.CategoryDirection
 
 val CategoryDirection.icon: ImageVector
     get() = when (this) {
-        CategoryDirection.Credit -> Icons.Filled.ArrowCircleUp
-        CategoryDirection.Debit -> Icons.Filled.ArrowCircleDown
+        CategoryDirection.Credit -> Icons.Filled.AddCircle
+        CategoryDirection.Debit -> Icons.Filled.DoNotDisturbOn
     }
