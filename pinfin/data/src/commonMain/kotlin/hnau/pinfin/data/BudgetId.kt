b9495bb84f6e02afe7f241e3ef94cd9c4b921b14
Mark Zorkhin <mark.zorikhin@dif.tech>
2025-05-31 22:20:41 +0300

    BuildgetId again value class

--- pinfin/data/src/commonMain/kotlin/hnau/pinfin/data/BudgetId.kt
+++ pinfin/data/src/commonMain/kotlin/hnau/pinfin/data/BudgetId.kt
@@ -3,16 +3,16 @@ package hnau.pinfin.data
 import hnau.common.kotlin.mapper.Mapper
 import hnau.common.kotlin.mapper.plus
 import hnau.common.kotlin.mapper.stringToUuid
-import hnau.common.kotlin.serialization.MappingKSerializer
 import hnau.common.kotlin.serialization.UuidSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.serializer
+import kotlin.jvm.JvmInline
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
-@Serializable(BudgetId.Serializer::class)
-data class BudgetId(
+@Serializable
+@JvmInline
+value class BudgetId(
     @Serializable(UuidSerializer::class)
     val id: Uuid,
 ) : Comparable<BudgetId> {
@@ -21,11 +21,6 @@ data class BudgetId(
         other: BudgetId,
     ): Int = id.compareTo(
         other = other.id,
-    )
-
-    data object Serializer : MappingKSerializer<String, BudgetId>(
-        base = String.serializer(),
-        mapper = stringMapper,
     )
 
     companion object {
