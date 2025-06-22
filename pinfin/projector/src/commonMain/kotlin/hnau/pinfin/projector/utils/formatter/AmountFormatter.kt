ad57c2bf2bb7af63b0132cb1cab304bcb86f7bf3
Mark Zorikhin <hnau256@gmail.com>
2025-06-22 23:04:09 +0300

    PF-4: amount formatting

--- pinfin/projector/src/commonMain/kotlin/hnau/pinfin/projector/utils/formatter/AmountFormatter.kt
+++ pinfin/projector/src/commonMain/kotlin/hnau/pinfin/projector/utils/formatter/AmountFormatter.kt
@@ -16,21 +16,42 @@ interface AmountFormatter {
 
         val test: AmountFormatter = object : AmountFormatter {
 
-            private val factor = 100f
+            private val factor = 100
 
             override fun format(
                 amount: Amount,
             ): String = amount
                 .value
                 .toLong()
-                .div(factor)
-                .toString()
-                .dropLastWhile { it == '0' }
-                .removeSuffix(".")
+                .let {
+                    val count = (it / factor)
+                        .toString()
+                        .reversed()
+                        .windowed(
+                            size = 3,
+                            step = 3,
+                            partialWindows = true,
+                        )
+                        .joinToString(
+                            separator = '\u00A0'.toString(),
+                        )
+                        .reversed()
+                    val cents = (it % factor)
+                        .takeIf { it > 0 }
+                        ?.toString()
+                        ?.padEnd(2, '0')
+                    listOfNotNull(
+                        count,
+                        cents
+                    ).joinToString(
+                        separator = "."
+                    )
+                }
 
             override fun parse(
                 input: String,
             ): Amount? = input
+                .filterNot(Char::isWhitespace)
                 .toFloatOrNull()
                 ?.times(factor)
                 ?.toLong()
