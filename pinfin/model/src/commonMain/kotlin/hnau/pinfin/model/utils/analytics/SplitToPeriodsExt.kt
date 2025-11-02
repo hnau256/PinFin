5bad0fe2571fa57f5a2f2a4d73bb9af808df4236
Mark Zorikhin <hnau256@gmail.com>
2025-11-02 22:03:58 +0200

    splitToPeriods fix

--- pinfin/model/src/commonMain/kotlin/hnau/pinfin/model/utils/analytics/SplitToPeriodsExt.kt
+++ pinfin/model/src/commonMain/kotlin/hnau/pinfin/model/utils/analytics/SplitToPeriodsExt.kt
@@ -14,58 +14,56 @@ fun <T> NonEmptyList<T>.splitToPeriods(
     customStartOfOneOfPeriods: LocalDate?,
     duration: DatePeriod,
     extractDate: (T) -> LocalDate,
-): NonEmptyList<Pair<LocalDateRange, List<T>>> {
-    return tail
-        .fold(
-            initial = run {
-                val firstDate = extractDate(head)
-                var start = customStartOfOneOfPeriods ?: firstDate
-                while (start + duration <= firstDate) {
-                    start += duration
-                }
-                while (start > firstDate) {
-                    start -= duration
-                }
-                val last = start to nonEmptyListOf(head)
-                listOf<Pair<LocalDate, List<T>>>() to last
+): NonEmptyList<Pair<LocalDateRange, List<T>>> = tail
+    .fold(
+        initial = run {
+            val firstDate = extractDate(head)
+            var start = customStartOfOneOfPeriods ?: firstDate
+            while (start + duration <= firstDate) {
+                start += duration
             }
-        ) { (previous, last), item ->
-            val (lastRangeStart, lastTransactions) = last
-            val lastRange = lastRangeStart.toRange(duration)
-            val date = extractDate(item)
-            if (date in lastRange) {
-                val newTransactions = lastTransactions.toNonEmptyListOrNull().foldNullable(
-                    ifNull = { nonEmptyListOf(item) },
-                    ifNotNull = { it + item },
-                )
-                val newLast = lastRangeStart to newTransactions
-                return@fold previous to newLast
+            while (start > firstDate) {
+                start -= duration
             }
-            var newLastRangeStart: LocalDate = lastRangeStart + duration
-            val newPrevious = buildList {
-                addAll(previous)
-                add(last)
-                while (date !in newLastRangeStart.toRange(duration)) {
-                    add(newLastRangeStart to emptyList())
-                    newLastRangeStart += duration
-                }
-            }
-            val newLast = newLastRangeStart to nonEmptyListOf(item)
-            newPrevious to newLast
+            val last = start to nonEmptyListOf(head)
+            listOf<Pair<LocalDate, List<T>>>() to last
         }
-        .let { (previous, last) ->
-            previous
-                .toNonEmptyListOrNull()
-                .foldNullable(
-                    ifNull = { nonEmptyListOf(last) },
-                    ifNotNull = { it + last }
-                )
-                .map { (rangeStart, items) ->
-                    val range = rangeStart.toRange(duration)
-                    range to items
-                }
+    ) { (previous, last), item ->
+        val (lastRangeStart, lastTransactions) = last
+        val lastRange = lastRangeStart.toRange(duration)
+        val date = extractDate(item)
+        if (date in lastRange) {
+            val newTransactions = lastTransactions.toNonEmptyListOrNull().foldNullable(
+                ifNull = { nonEmptyListOf(item) },
+                ifNotNull = { it + item },
+            )
+            val newLast = lastRangeStart to newTransactions
+            return@fold previous to newLast
         }
-}
+        var newLastRangeStart: LocalDate = lastRangeStart + duration
+        val newPrevious = buildList {
+            addAll(previous)
+            add(last)
+            while (date !in newLastRangeStart.toRange(duration)) {
+                add(newLastRangeStart to emptyList())
+                newLastRangeStart += duration
+            }
+        }
+        val newLast = newLastRangeStart to nonEmptyListOf(item)
+        newPrevious to newLast
+    }
+    .let { (previous, last) ->
+        previous
+            .toNonEmptyListOrNull()
+            .foldNullable(
+                ifNull = { nonEmptyListOf(last) },
+                ifNotNull = { it + last }
+            )
+            .map { (rangeStart, items) ->
+                val range = rangeStart.toRange(duration)
+                range to items
+            }
+    }
 
 private val oneDay: DatePeriod = DatePeriod(days = 1)
 
