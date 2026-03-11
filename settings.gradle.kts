1cafa72479b1284caf9c973ab00bd2fcb26a45bf
Mark Zorikhin <hnau256@gmail.com>
2026-03-12 01:50:52 +0200

    org.hnau.settings:1.2.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,54 +10,9 @@ pluginManagement {
 }
 
 plugins {
-    id("org.hnau.settings") version "1.2.0"
+    id("org.hnau.plugin.settings") version "1.2.2"
 }
 
 hnau {
     groupId = "org.hnau.pinfin"
 }
-
-fun collectSubproject(
-    projectDir: File,
-    projectIdentifier: String = "",
-): List<String> {
-    fun collectProjectWithSubprojects(
-        dir: File,
-        projectIdentifier: String,
-    ): List<String> {
-        fun checkIsProject(
-            dir: File,
-            projectIdentifier: String,
-        ): Boolean {
-            if (projectIdentifier == ":plugins") {
-                return false
-            }
-            if (projectIdentifier == ":buildSrc") {
-                return false
-            }
-            return dir
-                .list()
-                ?.any { file -> file == "settings.gradle.kts" || file == "build.gradle.kts" }
-                ?: false
-        }
-
-        return when (checkIsProject(dir, projectIdentifier)) {
-            true -> listOf(projectIdentifier)
-            false -> collectSubproject(dir, projectIdentifier)
-        }
-    }
-
-    return projectDir
-        .listFiles()
-        .orEmpty()
-        .filter { it.exists() && it.isDirectory }
-        .flatMap { subdir ->
-            val subdirName = subdir.name
-            collectProjectWithSubprojects(
-                dir = subdir,
-                projectIdentifier = "$projectIdentifier:$subdirName",
-            )
-        }
-}
-
-include(collectSubproject(rootProject.projectDir))
